256f58327ea6559b9325443a49f2cfbb2d638518
alanbabypaul <alanbabypaul29@gmail.com>
2023-10-31 11:59:25 +0530

    color-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import { Button } from 'react-bootstrap';
+import './App.css'
 
 function App() {
+  const [bgColor, setBgColor] = useState('bg-dark');
+
+  const handleColorChange = (newColor) => {
+    setBgColor(newColor);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<div className={`text-white ${bgColor}`}>
+      <div className='d-flex justify-content-center align-items-center flex-column vh-100'>
+        <h1> Select Background Colors </h1>
+        <div>
+        <Button style={{backgroundColor:'purple'}} className='btn btn-light m-2' onClick={() => handleColorChange('bg-purple')}>PURPLE</Button>
+
+        <button style={{backgroundColor:'Blue'}} className='btn btn-light m-2' onClick={() => handleColorChange('bg-blue')}>BLUE</button>
+          <button  style={{backgroundColor:'green'}} className='btn btn-light m-2' onClick={() => handleColorChange('bg-green')}>GREEN</button>
+
+          
+          <button   style={{backgroundColor:'Red'}} className='btn btn-light m-2' onClick={() => handleColorChange('bg-red')}>RED</button>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default App
